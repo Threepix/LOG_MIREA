f9bba94005fe3fd40a41fc6fe3e1bbc8af02cc72
danoneeee <90916110+danoneeee@users.noreply.github.com>
2022-05-10 22:06:04 +0300

    делаю сортировку

--- yric.py
+++ yric.py
@@ -13,10 +13,33 @@ def evalString(key, readF, writeF):
                 writeF.write(finalString + "\n")
     readFile.seek(0)
 
+
 # GET USER KEYWORD
 userSynonyms = []
 userKeyword = input("What keyword are you searching? ")
 userKeyword = userKeyword.lower()
+
+# EVALUATE IF THE USER WANTS TO INPUT SYNONYMS
+while True:
+    searchSyn = input("Are you looking for synonyms as well? (Y/N) ")
+
+    if searchSyn == "N" or searchSyn == "n":
+        print("No synonyms")
+        break
+    elif searchSyn == "Y" or searchSyn == "y":
+        # Get synonyms from user
+        while True:
+            userSynonym = input("Enter synonym you also want to search (Enter 0 to stop) ")
+            userSynonym = userSynonym.lower()
+
+            if userSynonym == "0":
+                break
+            else:
+                userSynonyms.append(userSynonym)
+
+        break
+    else:
+        print("Input is invalid")
 
 # GET READ AND WRITE FILE
 while True:
@@ -33,3 +56,14 @@ while True:
 
 # WRITE KEYWORD SECTION TO FILE
 writeFile.write("KEYWORD: " + userKeyword.upper() + "\n\n")
+evalString(userKeyword, readFile, writeFile)
+
+synString = ", ".join(userSynonyms)
+
+# WRITE SYNONYMS SECTION TO FILE
+writeFile.write("\nSYNONYMS: " + synString.upper() + "\n\n")
+for k in userSynonyms:
+    evalString(k, readFile, writeFile)
+
+readFile.close()
+writeFile.close()
