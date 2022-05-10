d384dcb185615a1ccb6c7c30d5c94b646f93a692
danoneeee <90916110+danoneeee@users.noreply.github.com>
2022-05-11 02:15:38 +0300

    сортировочка делаитса

--- yric.py
+++ yric.py
@@ -1,4 +1,4 @@
-#первый код сортировка по вводимым значениям
+# первый код сортировка по вводимым значениям
 import datetime
 with open('log.txt') as file:
     array = [row.strip() for row in file]
@@ -8,25 +8,22 @@ with open('log.txt') as file:
         if word in i:
             print(i)
 
-#второй сортировка по времени возрастающая
+# второй сортировка по времени возрастающая
 array = sorted(array, key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=False)
 print(array)
 
-#второй сортировка по времени убывающая
+# второй сортировка по времени убывающая
 array = sorted(array, key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
 print(array)
 
-#вывести первые n строк
+# вывести первые n строк
 n = int(input())
 with open("log.txt", 'r') as f:
     for i in range(n):
         print(f.readline())
 
-#вывести последние n строк
+# вывести последние n строк
 n = int(input())
 f_read = open("log.txt", "r")
 last_line = f_read.readlines()[-n]
 print(last_line)
-
-#вывести строки за определённое время
-
