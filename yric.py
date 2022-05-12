1012bb5c693ff149ffb302c56fe52fefbdb4b389
yurisobolev <57149543+yurisobolev@users.noreply.github.com>
2022-05-13 01:52:23 +0300

    Update yric.py

--- yric.py
+++ yric.py
@@ -1,29 +1,43 @@
 # первый код сортировка по вводимым значениям
 import datetime
-with open('log.txt') as file:
+with open('log.log') as file:
     array = [row.strip() for row in file]
     word = input()
 
     for i in array:
         if word in i:
             print(i)
+# Второй код сортировка по времени от n до m часов            
+n = int(input())
+m = int(input())
+file = open('log.log', "r")
+for line in file:
+    while True:
+        true_line = line
+        time_str = line[22] + line[23]
+        time = (sum(map(int, time_str.split())))
+        if ((time >= n) and (time <= m)) or (time == n) or (time == m):
+            print(true_line)
+            break
+        else:
+            break
 
-# второй сортировка по времени возрастающая
+# Третий сортировка по времени возрастающая
 array = sorted(array, key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=False)
 print(array)
 
-# второй сортировка по времени убывающая
+# Четвёртая сортировка по времени убывающая
 array = sorted(array, key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
 print(array)
 
 # вывести первые n строк
 n = int(input())
-with open("log.txt", 'r') as f:
+with open("log.log", 'r') as f:
     for i in range(n):
         print(f.readline())
 
 # вывести последние n строк
 n = int(input())
-f_read = open("log.txt", "r")
+f_read = open("log.log", "r")
 last_line = f_read.readlines()[-n]
 print(last_line)
